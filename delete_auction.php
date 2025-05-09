e9401ee79d16726e71c5bf0c6ce8d3e0a77a64ee
M7med910 <krti144fg@gmail.com>
2025-05-10 02:21:46 +0300

    Update delete_auction.php

--- delete_auction.php
+++ delete_auction.php
@@ -5,6 +5,7 @@ $host = 'n11111111.mysql.database.azure.com';
 $dbname = 'newschema';
 $username = 'm';
 $password = '11111111nN';
+
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("فشل الاتصال: " . $conn->connect_error);
