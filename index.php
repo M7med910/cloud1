85d29ac84d69c9316c9fd1747e6320a2c53bcd94
M7med910 <krti144fg@gmail.com>
2025-05-10 01:32:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ require 'track-visit.php';
 if (isset($_SESSION['user_id'])) {
 $host = 'n11111111.mysql.database.azure.com';
 $dbname = 'newschema';
-port=3306;
 $username = 'm';
 $password = '11111111nN';
-ssl-mode=require;
     
     $conn = new mysqli($host, $username, $password, $dbname);
     if ($conn->connect_error) {
