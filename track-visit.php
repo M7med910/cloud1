48b1f21db4cec3b6141075750e43e99ba1694081
M7med910 <krti144fg@gmail.com>
2025-05-10 01:40:57 +0300

    Update track-visit.php

--- track-visit.php
+++ track-visit.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sql200.infinityfree.com';
-$dbname = 'if0_38543174_my_db';
-$username = 'if0_38543174';
-$password = 'MDH6WuEkvPyL';
+$host = 'n11111111.mysql.database.azure.com';
+$dbname = 'newschema';
+$username = 'm';
+$password = '11111111nN';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
