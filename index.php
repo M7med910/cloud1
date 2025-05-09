1c2373c4593dfc08a751c10d9341f7907d5e2f5a
M7med910 <krti144fg@gmail.com>
2025-05-10 01:28:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ session_start();
 require 'track-visit.php';
 
 if (isset($_SESSION['user_id'])) {
-    $host = 'sql200.infinityfree.com';
-    $dbname = 'if0_38543174_my_db';
-    $username = 'if0_38543174';
-    $password = 'MDH6WuEkvPyL';
+   $host = 'n11111111.mysql.database.azure.com';
+$dbname = 'newschema';
+port=3306;
+$username = 'm';
+$password = '11111111nN';
+ssl-mode=require;
     
     $conn = new mysqli($host, $username, $password, $dbname);
     if ($conn->connect_error) {
