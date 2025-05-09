eb283dbfcb36e7e5e01754334fb625adaeed88e9
M7med910 <krti144fg@gmail.com>
2025-05-10 01:45:07 +0300

    Update auction_details.php

--- auction_details.php
+++ auction_details.php
@@ -8,10 +8,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // الاتصال بقاعدة البيانات
-$host = 'sql200.infinityfree.com';
-$dbname = 'if0_38543174_my_db';
-$username = 'if0_38543174';
-$password = 'MDH6WuEkvPyL';
+$host = 'n11111111.mysql.database.azure.com';
+$dbname = 'newschema';
+$username = 'm';
+$password = '11111111nN';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
