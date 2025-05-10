d5510bdc9f961c248dbd7a0ee54bf9e12c06592b
M7med910 <krti144fg@gmail.com>
2025-05-10 19:23:01 +0000

    Update track-visit.php

--- track-visit.php
+++ track-visit.php
@@ -13,16 +13,6 @@ try {
     die("fفشل الاتصال بقاعدة البيانات.");
 }
 
-$ip = $_SERVER['REMOTE_ADDR'];
 
-// تحقق هل هذا الـ IP زار اليوم بالفعل
-$stmt = $pdo->prepare("SELECT COUNT(*) FROM visits WHERE ip_address = ? AND DATE(visit_time) = CURDATE()");
-$stmt->execute([$ip]);
-$already_visited = $stmt->fetchColumn();
-
-if ($already_visited == 0) {
-    // لم يُسجّل اليوم، نسجله الآن
-    $stmt = $pdo->prepare("INSERT INTO visits (ip_address) VALUES (?)");
-    $stmt->execute([$ip]);
 }
 ?>
