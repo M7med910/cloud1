734dd5ea5a94f10c48bae6fdb6ca61477abb9386
M7med910 <krti144fg@gmail.com>
2025-05-10 15:06:39 +0300

    Update track-visit.php

--- track-visit.php
+++ track-visit.php
@@ -1,17 +1,5 @@
 <?php
-$host = 'n11111111.mysql.database.azure.com';
-$dbname = 'newschema';
-$username = 'm';
-$password = '11111111nN';
 
-$conn = new mysqli($host, $username, $password, $dbname, 3306, '/path/to/ssl-cert.pem');
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("fفشل الاتصال بقاعدة البيانات.");
-}
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
